c9529b74125b9515e79b95ca26fdaf11a2da6ca0
Manh Bui <buimanh17112001@gmail.com>
2022-01-09 13:46:08 +0700

    update params

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get("/list", async (req, res, next) => {
   try {
     const { params } = req;
     let url =
-      "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=100&sortBy=market_cap&sortType=desc&convert=USD&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d,self_reported_circulating_supply,self_reported_market_cap";
+      "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=100&sortBy=market_cap&sortType=desc&convert=USD";
 
     if (params.crypto_type) url += `&cryptoType=${params.crypto_type}`;
     else url += `&cryptoType=all`;
